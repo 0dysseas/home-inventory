814b1523372f078fee0ff5e49b04d745fefe87b3
0dysseas <kaziolas.od@gmail.com>
2018-03-25 21:29:51 +0300

    Implemented authorization for comments endpoints

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'products/$', views.InventoryList.as_view()),
-    url(r'^products/(?P<pk>[0-9]+)/$', views.InventoryDetail.as_view()),
-    url(r'^products/(?P<pk>[0-9]+)/comments/$', views.CommentsList.as_view()),
+    url(r'^products/(?P<product_id>[0-9]+)/$', views.InventoryDetail.as_view()),
+    url(r'^products/(?P<product_id>[0-9]+)/comments/$', views.CommentsList.as_view()),
+    url(r'^products/(?P<product_id>[0-9]+)/comments/(?P<comment_id>[0-9]+)/$', views.CommentsDetail.as_view()),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -2,25 +2,41 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Product, Comments
 from .serializers import ProductSerializer, CommentSerializer
-from .permissions import IsAdminOrReadOnly
+from .permissions import IsAdminOrReadOnly, IsOwnerOrReadOnly
 
 
 class InventoryList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = (IsAdminOrReadOnly, )
+    lookup_url_kwarg = 'product_id'
 
 
 class InventoryDetail(generics.RetrieveUpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = (IsAdminOrReadOnly, )
+    lookup_url_kwarg = 'product_id'
 
 
 class CommentsList(generics.ListCreateAPIView):
-    queryset = Comments.objects.all()
     serializer_class = CommentSerializer
     permission_classes = (IsAuthenticatedOrReadOnly, )
+    lookup_url_kwarg = 'product_id'
 
     def perform_create(self, serializer):
-        serializer.save(created_by=self.request.user, product_id=self.kwargs['pk'])
+        serializer.save(created_by=self.request.user, product_id=self.kwargs['product_id'])
+
+    def get_queryset(self):
+        product = self.kwargs['product_id']
+        return Comments.objects.filter(product__id=product)
+
+
+class CommentsDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    lookup_url_kwarg = 'comment_id'
+
+    def get_queryset(self):
+        comment = self.kwargs['comment_id']
+        return Comments.objects.filter(id=comment)
